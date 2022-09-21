cb69d9dd14020e94f51f21e175d34304681151dd
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-09-21 07:16:43 +0100

    Fix linter errors

--- modules/ui.js
+++ modules/ui.js
@@ -1,14 +1,15 @@
 import Store from './store.js';
 
 export default class UI {
-    static displayBooks() {
-        const books = Store.getBooks();
-        books.forEach((book) => UI.addBookToList(book))
-    }
-    static addBookToList (book) {
-        const books = document.querySelector('.books');
-        const newBook = document.createElement('div');
-        newBook.innerHTML = `
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const books = document.querySelector('.books');
+    const newBook = document.createElement('div');
+    newBook.innerHTML = `
         <div>
         <div class="bookDiv">
         <h4 id="title" class="">${book.title} &nbsp; &nbsp; &nbsp; &nbsp; by &nbsp; &nbsp; &nbsp; &nbsp; ${book.author}</h4>
@@ -18,16 +19,18 @@ export default class UI {
         <hr class="hr">
         </div>
         `;
-        newBook.classList.add('newBook')
-        books.appendChild(newBook);
+    newBook.classList.add('newBook');
+    books.appendChild(newBook);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
     }
-    static deleteBook(el) {
-        if(el.classList.contains('delete')){
-            el.parentElement.parentElement.remove();
-        }
-    }
-    static clearFields() {
-        document.querySelector('.title').value = ''
-        document.querySelector('.author').value = ''
-    }
+  }
+
+  static clearFields() {
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
+  }
 }
--- script.js
+++ script.js
@@ -2,36 +2,36 @@ import Store from './modules/store.js';
 import UI from './modules/ui.js';
 
 class Book {
-    constructor (title, author, id = Math.floor(Math.random() * 1000000)) {
-        this.title = title;
-        this.author = author;
-        this.id = id;
-    }
+  constructor(title, author, id = Math.floor(Math.random() * 1000000)) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 }
 
 // STORE CLASS
 
-//EVENT TO DISPLAY BOOKS
+// EVENT TO DISPLAY BOOKS
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
-//EVENT TO ADD A BOOK
+// EVENT TO ADD A BOOK
 document.querySelector('.bookForm').addEventListener('submit', (e) => {
-e.preventDefault();
-    //get form values
-    const titleInput = document.querySelector('.title').value
-    const authorInput = document.querySelector('.author').value
-    const book = new Book(titleInput, authorInput)
-    //ADD BOOK TO LIST
-    UI.addBookToList(book)
-    //ADD BOOK TO STORE
-    Store.addBook(book)
-    //CLEAR FIELDS
-    UI.clearFields()
-})
-//EVENT DELETE
+  e.preventDefault();
+  // get form values
+  const titleInput = document.querySelector('.title').value;
+  const authorInput = document.querySelector('.author').value;
+  const book = new Book(titleInput, authorInput);
+  // ADD BOOK TO LIST
+  UI.addBookToList(book);
+  // ADD BOOK TO STORE
+  Store.addBook(book);
+  // CLEAR FIELDS
+  UI.clearFields();
+});
+// EVENT DELETE
 document.querySelector('.books').addEventListener('click', (e) => {
-    if (e.target.className === 'delete') {
-        const id = e.target.previousElementSibling.innerText;
-        Store.removeBook(id)
-        UI.deleteBook(e.target)
-      }
-})
+  if (e.target.className === 'delete') {
+    const id = e.target.previousElementSibling.innerText;
+    Store.removeBook(id);
+    UI.deleteBook(e.target);
+  }
+});
